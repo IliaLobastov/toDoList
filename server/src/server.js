61d3c445cf65e4523496aa7a85072ae8a76ad153
VladimirKuznetsov1990 <fixer2026@gmail.com>
2025-03-25 16:08:27 +0300

    newBranchFromVladimir

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
-
+//bla bla bla
 app.listen(PORT, () => {
   console.log(`Сервер запущен на ${PORT} порту`);
 });
